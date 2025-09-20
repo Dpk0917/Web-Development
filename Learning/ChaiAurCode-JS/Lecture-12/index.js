f29cc3c4061b8557c5498cb914b01997b6dae435
Deepak Singh <dpk0917@gmail.com>
2025-09-21 02:06:37 +0530

    Lecture-12

---
+++ Learning/ChaiAurCode-JS/Lecture-12/index.js
@@ -0,0 +1,31 @@
+// const score=400;
+// console.log(score);
+
+// const balance= new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber=23.58444;
+// console.log(othernumber.toPrecision(3));
+
+// const hundreds=10000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,6,9));
+// console.log(Math.max(4,5,6,9));
+
+console.log(Math.random());  //always between 0-1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
