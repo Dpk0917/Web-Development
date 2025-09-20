851764361985b7e50c2bbbbced9bd7d6b0f250fe
Deepak Singh <dpk0917@gmail.com>
2025-09-20 12:07:06 +0530

    Lecture-7

---
+++ Learning/ChaiAurCode-JS/Lecture-7/index.js
@@ -0,0 +1,22 @@
+// let value = 3;
+// let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); //2 to the power 3
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+// let str = "Deepak";
+// let str2 = "Singh";
+// console.log(str + " " + str2);
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"  
+// console.log(1 + 2 + "2"); // "32" 
+
+// console.log(+true);
+console.log(+"");
