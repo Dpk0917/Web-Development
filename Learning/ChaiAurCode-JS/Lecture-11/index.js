ad52deafb29668409287c70b8f9d8576eba38f0b
Deepak Singh <dpk0917@gmail.com>
2025-09-21 01:33:08 +0530

    Lecture-11

---
+++ Learning/ChaiAurCode-JS/Lecture-11/index.js
@@ -0,0 +1,32 @@
+// const name="Deepak";
+// const repoCount=520;
+
+// // console.log(name+repoCount+"Value"); outdated
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gamename=new String('DeepakFF');
+
+// console.log(gamename);
+
+// console.log(gamename[0]);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('k'));
+
+
+// const anotherString=gamename.slice(-6,5); //piche se start krega 
+
+// console.log(anotherString);
+
+const newstring="     deepak     ";
+console.log(newstring);
+console.log(newstring.trim());
+
+const url="https://deepak.haha";
+console.log(url.replace('haha','sim'));
+console.log(url.includes('deepak'));   //return bool
+
+console.log(url.split('.')); //convert inro array on then basis of ". "
