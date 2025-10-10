5069baea145d6e925b9653ebb4901df8200eb532
Deepak Singh <dpk0917@gmail.com>
2025-10-11 01:53:19 +0530

    Adding files

--- JanMarshal-Next/learn-next.js/app/dashboard/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-type Params = Promise<{ params: { slug: string } }>;
-
-
-
-export default async function Dashboard({ params }: { params: { slug: string } }) {
-  const { slug } =  await params;
-  return <h1>Hello World from slug route: {slug}</h1>;
-}
